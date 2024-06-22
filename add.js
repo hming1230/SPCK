680c270501ab34198c808892d772fc0aecdbbd8a
hming123 <nguyenlammon@gmail.com>
2024-06-22 19:26:44 +0700

    update2

--- add.js
+++ add.js
@@ -1,51 +1,42 @@
-const btn = document.querySelector('.btn')
-const back = document.querySelector('.back')
-const inputname = document.querySelector ('#name')
-// const inputdiscription =  document.querySelector ('#discription')
-const inputprice = document.querySelector ('#price')
-const inputimage = document.querySelector ('#image')
+const btn = document.querySelector('.btn');
+const back = document.querySelector('.back');
+const inputName = document.querySelector('#name');
+const inputPrice = document.querySelector('#price');
+const inputImage = document.querySelector('#image');
 
-btn.addEventListener('click',
-(e) =>{
-   e.preventDefault()
-const nameValue = inputname.value 
-// const discriptionValue = inputdiscription.value
-const priceValue = inputprice.value
-const imageValue = inputimage.value
-
- 
-newTask= {
-   name: nameValue,
-//    discription: discriptionValue,
-   price: priceValue,
-   image: imageValue
-
-}
-
-  fetch('https://657e76623e3f5b189463bb26.mockapi.io/project/place', {
-    method: 'POST',
-    headers: {'content-type':'application/json'},
-    // Send your data in the request body as JSON
-    body: JSON.stringify(newTask)
-  }).then(res => {
-    if (res.ok) {
-        return res.json();
-    }
- 
-    // handle error
-  }).then(task => {       window.location = "./index.html"
-
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
   
-  }).catch(error => {
-    // handle error
+  const nameValue = inputName.value;
+  const priceValue = inputPrice.value;
+  const imageValue = inputImage.value;
+
+  const newTask = {
+    name: nameValue,
+    price: priceValue,
+    image: imageValue
+  };
+
+  fetch('https://657e76623e3f5b189463bb26.mockapi.io/project/special-package', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newTask)
   })
-;
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return res.json();
+    })
+    .then(task => {
+      window.location = "./index.html";
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+});
 
-
-})  
-back.addEventListener("click",
-(back)=>{
-    back.preventDefault();
-    window.location = "./index.html"
-}
-)
+back.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.location = "./index.html";
+});
--- index.js
+++ index.js
@@ -21,52 +21,49 @@ fetch("https://657e76623e3f5b189463bb26.mockapi.io/project/special-package", {
   })
   .then((places) => {
     console.log(places);
-    places.forEach((place) => {
+    places.forEach((placez) => {
       const productitem = document.createElement("div");
       productitem.classList.add("col-md-4", "col-sm-6");
 
       productitem.innerHTML = `
-            <img src="${place.image}" alt="package-place">
-            <div class="single-package-item-txt">
-                <h3>${place.name} <span class="pull-right">${place.price}</span></h3>
-                <div class="packages-para">
-                    <p>
-                        <span><i class="fa fa-angle-right"></i> 5 days 6 nights</span>
-                        <i class="fa fa-angle-right"></i>  5 star accommodation
-                    </p>
-                    <p>
-                        <span><i class="fa fa-angle-right"></i>  transportation</span>
-                        <i class="fa fa-angle-right"></i>  food facilities
-                    </p>
-                </div><!--/.packages-para-->
-                <div class="packages-review">
-                    <p>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <span>2544 review</span>
-                    </p>
-                </div><!--/.packages-review-->
-                <div class="about-btn">
-                    <button class="about-view packages-btn">book now</button>
-                </div><!--/.about-btn-->
-            </div><!--/.single-package-item-txt-->
-        `;
-      row.appendChild(productitem);
+        <img src="${placez.image}" alt="package-place">
+        <div class="single-package-item-txt">
+            <h3>${placez.name} <span class="pull-right">${placez.price}</span></h3>
+            <div class="packages-para">
+                <p>
+                    <span><i class="fa fa-angle-right"></i> 5 days 6 nights</span>
+                    <i class="fa fa-angle-right"></i>  5 star accommodation
+                </p>
+                <p>
+                    <span><i class="fa fa-angle-right"></i>  transportation</span>
+                    <i class="fa fa-angle-right"></i>  food facilities
+                </p>
+            </div>
+            <div class="packages-review">
+                <p>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <span>2544 review</span>
+                </p>
+            </div>
+            <div class="about-btn">
+                <button class="about-view packages-btn">book now</button>
+            </div>
+        </div>
+      `;
+      row222.appendChild(productitem);
     });
   })
   .catch((error) => {
-   
+    console.error("There was a problem with the fetch operation:", error);
   });
 
- const add = document.querySelector("#btnadd")
+const add = document.querySelector("#btnadd");
 
-//  add.addEventListener("click",(add)=>{
-//   add.preventDefault()
-//   window.location = "./add.html"
-//  }
-
-//  )
-
+add.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location = "./add.html";
+});
